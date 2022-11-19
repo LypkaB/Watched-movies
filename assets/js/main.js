03346dc1c217fd9a3fbb1df9179a169e064344d4
LypkaB <lpk.bdn@gmail.com>
2022-11-19 22:00:00 +0200

    Create obj «personalMovieDB» and add new fields to it

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,23 @@
 'use strict';
 
 window.addEventListener('DOMContentLoaded', () => {
-    
+    const numberOfFilms = +prompt('How many films have you already watched?', '');
+
+    const personalMovieDB = {
+        count: numberOfFilms,
+        movies: {},
+        actors: {},
+        genres: [],
+        privat: false
+    };
+
+    const firstQuestionLastWatchedMovie = prompt('One of the last movies you watched?', '');
+    const firstQuestionMovieRate = prompt('How much would you rate it?', '');
+    const secondQuestionLastWatchedMovie = prompt('One of the last movies you watched?', '');
+    const secondQuestionRateMovie = prompt('How much would you rate it?', '');
+
+    personalMovieDB.movies[firstQuestionLastWatchedMovie] = firstQuestionMovieRate;
+    personalMovieDB.movies[secondQuestionLastWatchedMovie] = secondQuestionRateMovie;
+
+    console.log(personalMovieDB.movies);
 })
